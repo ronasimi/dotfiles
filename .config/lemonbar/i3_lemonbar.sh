c429e20e2916e05d4151b60130920bae064b7971
Ron Asimi <ron.asimi@gmail.com>
2018-11-13 21:25:14 -0500

    remove mute check from lemonbar.sh - now handled in parser

--- .config/lemonbar/i3_lemonbar.sh
+++ .config/lemonbar/i3_lemonbar.sh
@@ -39,7 +39,7 @@ conky -c $(dirname $0)/i3_lemonbar_conky > "${panel_fifo}" &
 # Volume, "VOL"
 while read -r; do
 
-amixer get Master | grep "${snd_cha}" | awk -F'[]%[]' '/%/ {if ($7 == "off") {print "VOL×\n"} else {printf "VOL%d%%\n",$2}}' > "${panel_fifo}" &
+amixer get Master | grep "${snd_cha}" | awk -F'[]%[]' '/%/ {printf "VOL%d%%\n",$2}' > "${panel_fifo}" &
 
 done < <(echo && stdbuf -oL alsactl monitor pulse) &
 

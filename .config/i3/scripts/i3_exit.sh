5c40139f42a022021f8759a77c6157cb96bc741d
Ron Asimi <ron.asimi@gmail.com>
2018-12-06 20:56:49 -0500

    removed unecessary pkill from i3_exit.sh

--- .config/i3/scripts/i3_exit.sh
+++ .config/i3/scripts/i3_exit.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 ## kill lemonbar and daemons before exiting i3
 pkill lemonbar &&
-pkill i3_lemonbar.sh &&
-pkill i3_lemonbar_parser.sh &&
-pkill conky &&
-pkill feh &&
 pkill thunar &&
 pkill urxvtd &&
+pkill compton &&
 i3-msg exit

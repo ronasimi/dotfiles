359e99777d73f43d814959a1de60af67f742727d
Ron Asimi <ron.asimi@gmail.com>
2018-12-16 12:00:13 -0500

    update notifications in battery status script

--- .config/lemonbar/scripts/battime.sh
+++ .config/lemonbar/scripts/battime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 charging=$(acpi | awk '{print $3}' | sed 's/,//g')
 if [ "${charging}" == "Unknown" ]; then
-        (notify-send -u low "On AC" "System is on AC power")
+        (notify-send -u low "On AC" "System is on AC power and fully charged")
 elif [ "${charging}" == "Discharging" ]; then
         (notify-send -u normal "Battery discharging" "$(acpi | awk '{print $5}') remaining until discharged.")
 elif [ "${charging}" == "Charging" ]; then

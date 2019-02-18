01eea3164b078e071318b10570623f0de922b320
Ron Asimi <ron.asimi@gmail.com>
2019-02-18 00:18:10 -0500

    remove background color and separator for window icon - merge with title

--- home/.config/lemonbar/i3_lemonbar_parser.sh
+++ home/.config/lemonbar/i3_lemonbar_parser.sh
@@ -13,7 +13,7 @@
 . $(dirname $0)/i3_lemonbar_config
 
 # min init
-title="%{F${color_head} B${color_sec_b1}}${sep_right}%{F${color_icon} B${color_sec_b1} T2} ${icon_prog} %{F${color_sec_b1}}%{B- T1}${sep_right}%{F${color_title}} ${title}"
+title="%{F${color_head} B${color_sec_b2}}${sep_right}%{F${color_icon} B${color_sec_b2} T2} ${icon_prog}%{B- T1}%{F${color_title} T1} ${title}"
 
 # parser
 
@@ -85,8 +85,7 @@ while read -r line ; do
                 WIN*)
                         # window title
                         title=$(xprop -id ${line#???} | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
-
-                        title="%{F${color_head} B${color_sec_b1}}${sep_right}%{F${color_icon} B${color_sec_b1} T2} ${icon_prog} %{F${color_sec_b1}}%{B- T1}${sep_right}%{F${color_title}} ${title}"
+			title="%{F${color_head} B${color_sec_b2}}${sep_right}%{F${color_icon} B${color_sec_b2} T2} ${icon_prog}%{B- T1}%{F${color_title} T1} ${title}"
                         ;;
                 UPD*)
                         # Updates

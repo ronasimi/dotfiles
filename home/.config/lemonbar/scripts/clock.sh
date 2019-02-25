4712ed35306ec524b3e439d373867d0660136c9a
Ron Asimi <ron.asimi@gmail.com>
2019-02-25 11:58:57 -0500

    fix clock script - lower cpu usage considerably

--- home/.config/lemonbar/scripts/clock.sh
+++ home/.config/lemonbar/scripts/clock.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+# This loop will fill a buffer with our infos, and output it to stdout.
+# "date" output is checked every 3 seconds, but an event is only
+# generated if the output changed compared to the previous run."
 
 uniq_linebuffered() {
   awk '$0 != l { print ; l=$0 ; fflush(); }' "$@"
   }
 
-# This loop will fill a buffer with our infos, and output it to stdout.
-while true ; do
-   # "date" output is checked once a second, but an event is only
-   # generated if the output changed compared to the previous run.
-stdbuf -oL date +"%R"
-   sleep 3 || break
-done > >(uniq_linebuffered) &
+tm() {
+  date +%M; sleep 1; tm;
+  };tm | (uniq_linebuffered)

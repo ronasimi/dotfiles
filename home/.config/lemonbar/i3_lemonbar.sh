e286f02e86cf55dd9adc8c1254a5abdf9bcb73bb
Ron Asimi <ron.asimi@gmail.com>
2019-03-06 14:57:43 -0500

    small tweaks to lemonbar

--- home/.config/lemonbar/i3_lemonbar.sh
+++ home/.config/lemonbar/i3_lemonbar.sh
@@ -28,7 +28,7 @@ mkfifo "${panel_fifo}"
 # Window title, "WIN"
 while read -r; do
 
-        (xprop -root _NET_ACTIVE_WINDOW | sed -un 's/.*\(0x.*\)/WIN\1/p' > "${panel_fifo}") &
+        (xprop -root _NET_ACTIVE_WINDOW | sed -un 's/.*\(0x.*\)/WIN\1/p' > "${panel_fifo}")
 
 done < <(echo && i3-msg -t subscribe -m '[ "window", "workspace" ]') &
 
@@ -38,7 +38,7 @@ cnt_update="${upd_timer}"
 
 while :; do
         if [ $((cnt_update++)) -ge "${upd_timer}" ]; then
-                printf "%s%s\n" "UPD" "$("$(dirname $0)"/scripts/updates.sh)" > "${panel_fifo}" &
+                printf "%s%s\n" "UPD" "$("$(dirname $0)"/scripts/updates.sh)" > "${panel_fifo}"
                 cnt_update=0
         fi
 
@@ -52,7 +52,7 @@ cnt_mail="${mail_timer}"
 
 while :; do
         if [ $((cnt_mail++)) -ge "${mail_timer}" ]; then
-                printf "%s%s\n" "GMA" "$("$(dirname $0)"/scripts/gmail.sh)" > "${panel_fifo}" &
+                printf "%s%s\n" "GMA" "$("$(dirname $0)"/scripts/gmail.sh)" > "${panel_fifo}"
                 cnt_mail=0
         fi
 
@@ -63,29 +63,29 @@ done &
 # Backlight, "BRI"
 while read -r; do
 
-        (xbacklight -get | awk '{print "BRI" $1}' > "${panel_fifo}") &
+        (xbacklight -get | awk '{print "BRI" $1}' > "${panel_fifo}")
 
 done < <(echo && inotifywait -m -e modify /sys/class/backlight/acpi_video0/actual_brightness /sys/class/backlight/intel_backlight/actual_brightness -e open /sys/class/power_supply/AC/uevent) &
 
 # Volume, "MUT", "VOL"
 while read -r; do
 
-        (pamixer --get-volume | awk '{print "VOL" $1}' > "${panel_fifo}") &
+        (pamixer --get-volume | awk '{print "VOL" $1}' > "${panel_fifo}")
 
 done < <(echo && inotifywait -m /dev/snd/controlC0) &
 
 # Network, "ETH", "WFI"
 while read -r; do
 
-        (nmcli -t | grep enp0s25: | awk '{print "ETH" $2}' > "${panel_fifo}") &
-        (nmcli -t | grep wlp3s0: | awk '{print "WFI" $2}' > "${panel_fifo}") &
+        (nmcli -t | grep enp0s25: | awk '{print "ETH" $2}' > "${panel_fifo}")
+        (nmcli -t | grep wlp3s0: | awk '{print "WFI" $2}' > "${panel_fifo}")
 
 done < <(echo && nmcli m) &
 
 # Battery, "BAT"
 while read -r; do
 
-        (acpi -b | awk '{print "BAT" $4}' | tr -d '%,' > "${panel_fifo}") &
+        (acpi -b | awk '{print "BAT" $4}' | tr -d '%,' > "${panel_fifo}")
 
 done < <(echo && upower --monitor) &
 
@@ -94,9 +94,9 @@ done < <(echo && upower --monitor) &
 while read -r; do
 
         if [ "${res_w}" -gt 1024 ]; then
-        	(date +'%a %b %d %R' | tee >(awk ' { print "DAY"$1,$2,$3 } ') >(awk ' {print "CLK"$4 } ') >/dev/null) > "${panel_fifo}" &
+        	(date +'%a %b %d %R' | tee >(awk ' { print "DAY"$1,$2,$3 } ') >(awk ' {print "CLK"$4 } ') >/dev/null) > "${panel_fifo}"
         else
-        	(date +'%a %b %d %R' | tee >(awk ' { print "DAY"$2,$3 } ') >(awk ' {print "CLK"$4 } ') >/dev/null) > "${panel_fifo}" &
+        	(date +'%a %b %d %R' | tee >(awk ' { print "DAY"$2,$3 } ') >(awk ' {print "CLK"$4 } ') >/dev/null) > "${panel_fifo}"
         fi
 
 done < <(echo && "$(dirname $0)"/scripts/timer.sh) &

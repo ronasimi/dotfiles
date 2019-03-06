c4da2e1c83233f66635135854738fd194eb6d2f8
Ron Asimi <ron.asimi@gmail.com>
2019-03-05 19:43:53 -0500

    Revert "use c executables for time and date, run them through awk to only update the bar when changed."
    This reverts commit 31df234c83f08cfe73daa67609c11dec857d1505.

--- home/.config/lemonbar/i3_lemonbar.sh
+++ home/.config/lemonbar/i3_lemonbar.sh
@@ -91,8 +91,15 @@ done < <(echo && upower --monitor) &
 
 # date/time
 
-$(dirname $0)/scripts/date.sh > "${panel_fifo}" &
-$(dirname $0)/scripts/clock.sh > "${panel_fifo}" &
+while read -r; do
+
+        if [ ${res_w} -gt 1024 ]; then
+        	(date +'%a %b %d %R' | tee >(awk ' { print "DAY"$1,$2,$3 } ') >(awk ' {print "CLK"$4 } ') >/dev/null) > "${panel_fifo}"
+        else
+        	(date +'%a %b %d %R' | tee >(awk ' { print "DAY"$2,$3 } ') >(awk ' {print "CLK"$4 } ') >/dev/null) > "${panel_fifo}"
+        fi
+
+done < <(echo && $(dirname $0)/scripts/timer.sh) &
 
 
 #### LOOP FIFO

4d8e8b75d83e74c063572ba50196b8e0d3a28435
Ron Asimi <ron.asimi@gmail.com>
2019-03-16 21:20:32 -0400

    clean up i3 config

--- home/.config/i3/scripts/barcycle.sh
+++ home/.config/i3/scripts/barcycle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script to restart the bar, without restarting i3
 
-pkill i3_lemonbar.sh && ~/.config/lemonbar/i3_lemonbar.sh
+pkill lemonbar && pkill i3_lemonbar.sh && sleep 1 && bash ~/.config/lemonbar/i3_lemonbar.sh

1c7cf814d438689b78c1a054fc1c805bb641a8e4
Ron Asimi <ron.asimi@gmail.com>
2019-03-19 13:07:43 -0400

    add layout and script for workspace 1 - start chromium in tabbed layout with an incognito instance

---
+++ home/.config/i3/scripts/ws1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# First we append the saved layout of worspace N to workspace M
+i3-msg "workspace 1:; append_layout ~/.config/i3/layouts/ws1.json"
+
+# And finally we fill the containers with the programs they had
+(exec chromium &)
+(exec chromium --incognito &)

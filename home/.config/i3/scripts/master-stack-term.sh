25e09147e026156a20286b4c7f6dfe14344f1efd
Ron Asimi <ron.asimi@gmail.com>
2019-03-19 10:41:01 -0400

    add new saved layout for workspace 2, update script

--- home/.config/i3/scripts/master-stack-term.sh
+++ home/.config/i3/scripts/master-stack-term.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # First we append the saved layout of worspace N to workspace M
-i3-msg "workspace 2:; append_layout ~/.config/i3/layouts/master-stack.json"
+i3-msg "workspace 2:; append_layout ~/.config/i3/layouts/ws2.json"
 
 # And finally we fill the containers with the programs they had
+(urxvtc &)
 (urxvtc &)
 (urxvtc &)
 (urxvtc &)

ba7e8d6668bdcc75842c823651438073cfe5fa81
Ron Asimi <ron.asimi@gmail.com>
2019-03-19 11:37:16 -0400

    add layout for workspace 4 - atom and terminal.  rename workspace 2 script.  update i3 config to match all of this

---
+++ home/.config/i3/scripts/ws2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# First we append the saved layout of worspace N to workspace M
+i3-msg "workspace 2:; append_layout ~/.config/i3/layouts/ws2.json"
+
+# And finally we fill the containers with the programs they had
+(urxvtc &)
+(urxvtc &)
+(urxvtc &)
+(urxvtc &)
---
+++ home/.config/i3/scripts/ws4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# First we append the saved layout of worspace N to workspace M
+i3-msg "workspace 4:; append_layout ~/.config/i3/layouts/ws4.json"
+
+# And finally we fill the containers with the programs they had
+(atom &)
+(urxvtc -cd /home/ron/repos &)

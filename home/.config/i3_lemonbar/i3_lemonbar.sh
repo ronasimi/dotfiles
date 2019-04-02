f64c4e13b008f25bdf4442608b4315fa6aa1c46c
Ron Asimi <ron.asimi@gmail.com>
2019-04-02 16:23:02 -0400

    format lemonbar scripts

--- home/.config/i3_lemonbar/i3_lemonbar.sh
+++ home/.config/i3_lemonbar/i3_lemonbar.sh
@@ -10,9 +10,9 @@
 
 . "$(dirname $0)"/i3_lemonbar_config
 
-if [ "$(pgrep -cx "$(basename $0)")" -gt 1 ] ; then
-	printf "%s\n" "The status bar is already running." >&2
-	exit 1
+if [ "$(pgrep -cx "$(basename $0)")" -gt 1 ]; then
+  printf "%s\n" "The status bar is already running." >&2
+  exit 1
 fi
 
 trap 'trap - TERM; kill 0' INT TERM QUIT EXIT
@@ -23,32 +23,32 @@ mkfifo "${panel_fifo}"
 ### EVENTS METERS
 
 # i3 binding mode "MOD"
-(echo "MODinit" > "${panel_fifo}" && i3-msg -t subscribe -m '[ "mode" ]' | awk -F '"' '{print "MOD" $4; fflush(stdout)}' > "${panel_fifo}") &
+(echo "MODinit" >"${panel_fifo}" && i3-msg -t subscribe -m '[ "mode" ]' | awk -F '"' '{print "MOD" $4; fflush(stdout)}' >"${panel_fifo}") &
 
 # container layout, "LAY"
 while read -r; do
 
-	(echo "LAY$(i3-msg -t get_tree | jq -r 'recurse(.nodes[];.nodes!=null)|select(.nodes[].focused).layout')" > "${panel_fifo}")
+  (echo "LAY$(i3-msg -t get_tree | jq -r 'recurse(.nodes[];.nodes!=null)|select(.nodes[].focused).layout')" >"${panel_fifo}")
 
 done < <(echo && i3-msg -t subscribe -m '[ "window", "workspace", "binding" ]') &
 
 # i3 Workspaces, "WSP"
-"$(dirname $0)"/scripts/workspaces.pl > "${panel_fifo}" &
+"$(dirname $0)"/scripts/workspaces.pl >"${panel_fifo}" &
 
 # Window title, "WIN"
-(xtitle -i -s | awk '{print "WIN" $0; fflush(stdout)}' > "${panel_fifo}") &
+(xtitle -i -s | awk '{print "WIN" $0; fflush(stdout)}' >"${panel_fifo}") &
 
 # Updates, "UPD"
 ### Update check interval
 cnt_update="${upd_timer}"
 
 while :; do
-	if [ $((cnt_update++)) -ge "${upd_timer}" ]; then
-		printf "%s%s\n" "UPD" "$("$(dirname $0)"/scripts/updates.sh)" > "${panel_fifo}"
-		cnt_update=0
-	fi
+  if [ $((cnt_update++)) -ge "${upd_timer}" ]; then
+    printf "%s%s\n" "UPD" "$("$(dirname $0)"/scripts/updates.sh)" >"${panel_fifo}"
+    cnt_update=0
+  fi
 
-	sleep 60
+  sleep 60
 
 done &
 
@@ -57,61 +57,61 @@ done &
 cnt_mail="${mail_timer}"
 
 while :; do
-	if [ $((cnt_mail++)) -ge "${mail_timer}" ]; then
-		printf "%s%s\n" "GMA" "$("$(dirname $0)"/scripts/gmail.sh)" > "${panel_fifo}"
-		cnt_mail=0
-	fi
+  if [ $((cnt_mail++)) -ge "${mail_timer}" ]; then
+    printf "%s%s\n" "GMA" "$("$(dirname $0)"/scripts/gmail.sh)" >"${panel_fifo}"
+    cnt_mail=0
+  fi
 
-	sleep 60
+  sleep 60
 
 done &
 
 # Backlight, "BRI"
 while read -r; do
 
-	(xbacklight -get | awk '{print "BRI" $1}' > "${panel_fifo}")
+  (xbacklight -get | awk '{print "BRI" $1}' >"${panel_fifo}")
 
 done < <(echo && inotifywait -m -e modify /sys/class/backlight/acpi_video0/actual_brightness /sys/class/backlight/intel_backlight/actual_brightness -e open /sys/class/power_supply/AC/uevent) &
 
 # Volume, "MUT", "VOL"
 while read -r; do
 
-	(pamixer --get-volume | awk '{print "VOL" $1}' > "${panel_fifo}")
+  (pamixer --get-volume | awk '{print "VOL" $1}' >"${panel_fifo}")
 
 done < <(echo && stdbuf -oL alsactl monitor pulse) &
 
 # Network, "ETH", "WFI"
 while read -r; do
 
-	(nmcli -t | grep enp0s25: | awk '{print "ETH" $2}' > "${panel_fifo}")
-	(nmcli -t | grep wlp3s0: | awk '{print "WFI" $2}' > "${panel_fifo}")
+  (nmcli -t | grep enp0s25: | awk '{print "ETH" $2}' >"${panel_fifo}")
+  (nmcli -t | grep wlp3s0: | awk '{print "WFI" $2}' >"${panel_fifo}")
 
 done < <(echo && nmcli m) &
 
 # Battery, "BAT"
 while read -r; do
 
-	(acpi -b | awk '{print "BAT" $4}' | tr -d '%,' > "${panel_fifo}")
+  (acpi -b | awk '{print "BAT" $4}' | tr -d '%,' >"${panel_fifo}")
 
 done < <(echo && upower --monitor) &
 
 # date/time
 
-"$(dirname $0)"/scripts/clk > "${panel_fifo}" &
-"$(dirname $0)"/scripts/day > "${panel_fifo}" &
+"$(dirname $0)"/scripts/clk >"${panel_fifo}" &
+"$(dirname $0)"/scripts/day >"${panel_fifo}" &
 
 #### LOOP FIFO
 
-(cat "${panel_fifo}" | "$(dirname $0)"/i3_lemonbar_parser.sh \
-	| lemonbar -p -d -f "${font}" -f "${iconfont}" -a "${clickables}" -g "${geometry}" -B "${color_back}" -F "${color_fore}" | sh) &
+(cat "${panel_fifo}" | "$(dirname $0)"/i3_lemonbar_parser.sh |
+  lemonbar -p -d -f "${font}" -f "${iconfont}" -a "${clickables}" -g "${geometry}" -B "${color_back}" -F "${color_fore}" | sh) &
 
 #### Keep lemonbar below fullscreen windows
 
 tries_left=20
-while [ -z "$wid" -a "$tries_left" -gt 0 ] ; do
-	sleep 0.05
-	xdo above -t $(xwininfo -root -children | grep -E -o "0x[[:xdigit:]]+" | tail -1) $(xdo id -a bar)
-	tries_left=$((tries_left - 1))
+while [ -z "$wid" -a "$tries_left" -gt 0 ]; do
+  sleep 0.05
+  xdo above -t $(xwininfo -root -children | grep -E -o "0x[[:xdigit:]]+" | tail -1) $(xdo id -a bar)
+  tries_left=$((tries_left - 1))
 done
 
 wait

24a822c9150dc0dfc88bffff38b3d0aa0ca576ea
Ron Asimi <ron.asimi@gmail.com>
2019-04-02 13:32:06 -0400

    lemonbar - update updates.sh

--- home/.config/i3_lemonbar/scripts/updates.sh
+++ home/.config/i3_lemonbar/scripts/updates.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # check for package and aur updates
+wget -q --spider http://google.com
 
+if [ $? -eq 0 ]; then
   pkg=$(checkupdates 2>/dev/null | sed '/^\s*$/d' | wc -l)
   aur=$(pikaur -Qua 2>/dev/null | sed '/^\s*$/d' | wc -l)
   upd=$(($pkg + $aur))
+else
+  upd="err"
+fi
 
-  echo ${upd}
+echo ${upd}

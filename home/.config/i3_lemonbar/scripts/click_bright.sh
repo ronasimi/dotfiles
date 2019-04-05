d282e7755c59a7b24b83e3c49fbde34c68e958f8
Ron Asimi <ron.asimi@gmail.com>
2019-04-05 09:38:54 -0400

    remove unneeded scripts from lemonbar

--- home/.config/i3_lemonbar/scripts/click_bright.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-(notify-send -u low "Backlight level" "Panel backlight at $(xbacklight -get)%")
--- home/.config/i3_lemonbar/scripts/click_vol.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-(notify-send -u low "Output volume" "Output volume at $(pamixer --get-volume)%")

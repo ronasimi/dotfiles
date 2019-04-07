e60c3b5cf5c1866ff5e510b3240f2ce701922574
Ron Asimi <ron.asimi@gmail.com>
2019-04-07 19:49:59 -0400

    update barcycle.sh

--- home/.config/i3/scripts/barcycle.sh
+++ home/.config/i3/scripts/barcycle.sh
@@ -2,5 +2,4 @@
 # Script to restart the bar, without restarting i3
 
 pkill lemonbar
-$HOME/.config/i3_lemonbar/i3_lemonbar.sh &
-2>/dev/null >/dev/null
+$HOME/.config/i3_lemonbar/i3_lemonbar.sh 2>/dev/null >/dev/null &

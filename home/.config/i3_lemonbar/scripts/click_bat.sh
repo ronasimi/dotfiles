8910aacb319e1990c473482cd7dadcb2149305b3
Ron Asimi <ron.asimi@gmail.com>
2019-04-06 22:20:33 -0400

    cleanup on battery notification

--- home/.config/i3_lemonbar/scripts/click_bat.sh
+++ home/.config/i3_lemonbar/scripts/click_bat.sh
@@ -4,9 +4,9 @@ timeleft=$(acpi | awk '{print $5}')
 msgId="991051"
 
 if [ "${charging}" == "Unknown" ]; then
-        (dunstify -a "clickBat" -u low -r "$msgId" "On AC" "System is on AC power and not charging.\nCurrently at $(acpi -b | awk '{print $4}')")
+        (dunstify -a "clickBat" -u low -r "$msgId" "On AC" "System is on AC power and not charging.\nCurrently at $(acpi -b | awk '{print $4}' | tr -d '%,')%")
 elif [ "${charging}" == "Discharging" ]; then
-        (dunstify -a "clickBat" -u normal -r "$msgId" "Battery discharging" "$timeleft remaining until discharged.\nCurrently at $(acpi -b | awk '{print $4}')")
+        (dunstify -a "clickBat" -u normal -r "$msgId" "Battery discharging" "$timeleft remaining until discharged.\nCurrently at $(acpi -b | awk '{print $4}' | tr -d '%,')%")
 elif [ "${charging}" == "Charging" ]; then
-        (dunstify -a "clickBat" -u normal -r "$msgId" "Battery charging" "$timeleft remaining until charged.\nCurrently at $(acpi -b | awk '{print $4}')")
+        (dunstify -a "clickBat" -u normal -r "$msgId" "Battery charging" "$timeleft remaining until charged.\nCurrently at $(acpi -b | awk '{print $4}' | tr -d '%,')%")
 fi

195a6b91064f0df9ec30c5a9900920aecd480887
Ron Asimi <ron.asimi@gmail.com>
2019-04-29 19:15:43 -0400

    add layout for ws3 (file managers)

---
+++ home/.config/i3/scripts/ws3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# First we append the saved layout of worspace N to workspace M
+i3-msg "workspace 3:; append_layout ~/.config/i3/layouts/ws3.json"
+
+# And finally we fill the containers with the programs they had
+(exec urxvt -e ranger &)
+(exec thunar &)

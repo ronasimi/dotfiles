b0f693061dd054adc52dbbe460dfe43a46b72471
Ron Asimi <ron.asimi@gmail.com>
2019-08-01 15:02:15 -0400

    update dunstrc and wifi info script

--- home/.config/i3_lemonbar/scripts/click_wifi.sh
+++ home/.config/i3_lemonbar/scripts/click_wifi.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 wifion=$(nmcli -t | grep wlp3s0: | awk '{print $2}')
 wifiIP=$(ip addr show wlp3s0 | grep inet | awk ' { print $2 } ')
-ssid=$(nmcli -f ACTIVE,SSID dev wifi list | awk '$1=="yes" {$1=""; print $0} ')
+wifiqual=$(iwconfig wlp3s0 | sed -n -e 's/^[ \t]*//' -e '/Link/p')
+wifirate=$(iwconfig wlp3s0 | sed -n -e 's/^[ \t]*//' -e '/Bit/p')
+ssid=$(iwconfig wlp3s0 | grep ESSID | awk ' { print $4 } ')
 msgId="991053"
 
 if [ "${wifion}" == "connected" ]; then
-	(dunstify -a "clickWifi" -u low -r "$msgId" "WiFi connected" "SSID: $ssid\nCurrent IP addresses: $wifiIP")
+	(dunstify -a "clickWifi" -u low -t 10000 -r "$msgId" "WiFi connected" "$ssid\n$wifiqual\n$wifirate\nCurrent IP addresses: $wifiIP")
 else
 	(dunstify -a "clickWifi" -u normal -r "$msgId" "WiFi disconnected")
 fi

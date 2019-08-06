dfb7a899cf0426c651857dda33b394a1e2755a7b
Ron Asimi <ron.asimi@gmail.com>
2019-08-06 19:57:33 -0400

    update some spacing for lemonbar and finalize kitty.conf

--- home/.config/i3_lemonbar/i3_lemonbar_parser.sh
+++ home/.config/i3_lemonbar/i3_lemonbar_parser.sh
@@ -76,13 +76,13 @@ while read -r line; do
 
 		WIN*)
 			# window title
-			title="%{F${color_ina} B- T2}${sep_right}%{F${color_title} T1} ${line#???}%{T-}"
+			title="%{F${color_ina} B- T2}${sep_right} %{F${color_title} T1} ${line#???}%{T-}"
 			;;
 
 		UPD*)
 			# updates
 			if [ "${line#???}" != "0" ]; then
-				updates="%{F${color_upd} T2}${sep_left}%{F${color_icon_dark} B${color_upd}T3} ${icon_arch}%{T1} ${line#???}${stab}"
+				updates="%{F${color_upd} T2}${sep_left} %{F${color_icon_dark} B${color_upd}T3} ${icon_arch}%{T1} ${line#???}${stab}"
 			else
 				upd_cback=${color_sec_b1}
 				upd_cicon=${color_icon}
@@ -238,12 +238,12 @@ while read -r line; do
 
 		DAY*)
 			# date
-			date="%{F${color_sec_b1}T2}${sep_left}%{F${color_icon} B${color_sec_b1}} %{T3}${icon_cal}%{F${color_fore} T1} ${line#???}%{T-}"
+			date="%{F${color_sec_b1}T2}${sep_left}%{F${color_icon} B${color_sec_b1}} %{T3}${icon_cal} %{F${color_fore} T1} ${line#???}%{T-}"
 			;;
 
 		CLK*)
 			# time
-			time="%{F${color_sec_b2}T2}${sep_left}%{F${color_icon} B${color_sec_b2}} %{T3}${icon_clock}%{F${color_fore} T1} ${line#???}%{T-}"
+			time="%{F${color_sec_b2}T2}${sep_left}%{F${color_icon} B${color_sec_b2}} %{T3}${icon_clock} %{F${color_fore} T1} ${line#???}%{T-}"
 			;;
 
 	esac

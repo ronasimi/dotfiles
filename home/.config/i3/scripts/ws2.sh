b13e4d0142e8ea0851bfb67892ba10834d99e12d
Ron Asimi <ron.asimi@gmail.com>
2019-08-07 00:03:36 -0400

    update saved layout to use kitty for ws2

--- home/.config/i3/scripts/ws2.sh
+++ home/.config/i3/scripts/ws2.sh
@@ -4,7 +4,7 @@
 i3-msg "workspace 2:; append_layout ~/.config/i3/layouts/ws2.json"
 
 # And finally we fill the containers with the programs they had
-(urxvtc &)
-(urxvtc &)
-(urxvtc &)
-(urxvtc &)
+(kitty &)
+(kitty &)
+(kitty &)
+(kitty &)

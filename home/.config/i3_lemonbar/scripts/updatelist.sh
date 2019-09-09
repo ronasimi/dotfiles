327c2b6b5aac7ebbf20e818a92c140c0e528691d
Ron Asimi <ron.asimi@gmail.com>
2019-09-09 17:00:21 -0400

    update to volume and updates notifications

--- home/.config/i3_lemonbar/scripts/updatelist.sh
+++ home/.config/i3_lemonbar/scripts/updatelist.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Arbitrary but unique message id
+msgId="991075"
+dunstify -u low -t 30000 -r "$msgId" "Fetching update list..."
 repos=$(checkupdates 2>/dev/null && pikaur -Qua 2>/dev/null)
-dunstify -u low -t 20000 "Available Updates" "$repos"
+dunstify -u low -t 20000 -r "$msgId" "Available Updates:" "$repos"

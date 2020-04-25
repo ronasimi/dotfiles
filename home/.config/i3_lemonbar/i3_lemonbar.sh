d3c3d8830b8a05107ae7a440f7d809a8289954c3
Ron Asimi <ron.asimi@gmail.com>
2020-04-25 19:26:23 -0400

    use echo instead of awk to get volume into the pipe for lemonbar.

--- home/.config/i3_lemonbar/i3_lemonbar.sh
+++ home/.config/i3_lemonbar/i3_lemonbar.sh
@@ -67,7 +67,7 @@ done &
 # volume, "VOL"
 while read -r; do
 
-  (pamixer --get-volume-human | tr -d "%" | awk ' {print "VOL"$1} ') >"${panel_fifo}" &
+  (echo "VOL""$(pamixer --get-volume-human | tr -d "%")") >"${panel_fifo}" &
   "$(dirname $0)"/scripts/volindicator.sh &
 
 done < <(

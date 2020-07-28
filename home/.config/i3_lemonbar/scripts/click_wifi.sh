66685ebc4359a85b2e46f44ef4445a7c88d5ede4
Ron Asimi <ron.asimi@gmail.com>
2020-07-28 15:29:42 -0400

    update further for inter font

--- home/.config/i3_lemonbar/scripts/click_wifi.sh
+++ home/.config/i3_lemonbar/scripts/click_wifi.sh
@@ -7,7 +7,7 @@ ssid=$(iwconfig wlp3s0 | grep ESSID | cut -d '"' -f 2)
 msgId="991053"
 
 if [ "${wifion}" == "connected" ]; then
-	(dunstify -a "clickWifi" -u low -t 10000 -r "$msgId" "WiFi connected" "SSID: $ssid\nBit Rate: $wifirate\nIPv4 Address: $wifiIP4\nIPv6 Address: $wifiIP6")
+	(dunstify -a "clickWifi" -u low -t 10000 -r "$msgId" "WiFi connected" "SSID: $ssid\nBit Rate: $wifirate\nIPv4 Address: $wifiIP4\nIPv6 Address(es): $wifiIP6")
 else
 	(dunstify -a "clickWifi" -u normal -r "$msgId" "WiFi disconnected")
 fi

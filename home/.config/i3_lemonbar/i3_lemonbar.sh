abda6e1a029dc0478b511e075d88d2e999ee78ca
Ron Asimi <ron.asimi@gmail.com>
2020-08-03 21:20:20 -0400

    switch to source sans pro for interface/proportional font

--- home/.config/i3_lemonbar/i3_lemonbar.sh
+++ home/.config/i3_lemonbar/i3_lemonbar.sh
@@ -159,6 +159,6 @@ while read -r; do
 #### LOOP FIFO
 
 (cat "${panel_fifo}" | "$(dirname $0)"/i3_lemonbar_parser.sh |
-lemonbar -p -o -2 -f "${font}" -o 0 -f "${plfont}" -o -4 -f "${iconfont}" -o -1 -f "${titlefont}" -a "${clickables}" -g "${geometry}" -B "${color_back}" -F "${color_fore}" | sh) &
+lemonbar -p -o -2 -f "${font}" -o 0 -f "${plfont}" -o -4 -f "${iconfont}" -o -0 -f "${titlefont}" -a "${clickables}" -g "${geometry}" -B "${color_back}" -F "${color_fore}" | sh) &
 
 wait

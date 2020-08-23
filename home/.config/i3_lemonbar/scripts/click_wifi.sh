8d5613cb4573afcca0b8961fec3310520b7eab53
Ron Asimi <ron.asimi@gmail.com>
2020-08-22 23:05:18 -0400

    update lemonbar to track master

--- home/.config/i3_lemonbar/scripts/click_wifi.sh
+++ home/.config/i3_lemonbar/scripts/click_wifi.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-wifion=$(nmcli -t | grep wlp3s0: | awk '{print $2}')
-wifiIP=$(ip addr show wlp3s0 | grep inet | awk ' { print $2 } ')
-wifiqual=$(iwconfig wlp3s0 | sed -n -e 's/^[ \t]*//' -e '/Link/p')
-wifirate=$(iwconfig wlp3s0 | sed -n -e 's/^[ \t]*//' -e '/Bit/p')
-ssid=$(iwconfig wlp3s0 | grep ESSID | awk ' { print $4 } ')
+
+. "$(dirname $0)"/../i3_lemonbar_config
+
+wifion=$(nmcli -t | grep $wifi: | awk '{print $2}')
+wifiIP4=$(ip -4 addr show $wifi | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+wifiIP6=$(ip -6 addr show $wifi | grep -oP '(?<=inet6\s)[\da-f:]+')
+wifirate=$(iwconfig $wifi | grep Bit | cut -d '=' -f 2 | cut -d ' ' -f 1-2)
+ssid=$(iwconfig $wifi | grep ESSID | cut -d '"' -f 2)
 msgId="991053"
 
 if [ "${wifion}" == "connected" ]; then
-	(dunstify -a "clickWifi" -u low -t 10000 -r "$msgId" "WiFi connected" "$ssid\n$wifiqual\n$wifirate\nCurrent IP addresses: $wifiIP")
+	(dunstify -a "clickWifi" -u low -t 10000 -r "$msgId" "WiFi connected" "SSID: $ssid\nBit Rate: $wifirate\nIPv4 Address: $wifiIP4\nIPv6 Address(es): $wifiIP6")
 else
 	(dunstify -a "clickWifi" -u normal -r "$msgId" "WiFi disconnected")
 fi

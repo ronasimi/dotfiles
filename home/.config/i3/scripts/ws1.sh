c76cb27f884074f64ab4c4d7f88b7159bde894d2
Ron Asimi <ron.asimi@gmail.com>
2021-06-06 20:40:54 -0400

    switch to firefox for default browser

--- home/.config/i3/scripts/ws1.sh
+++ home/.config/i3/scripts/ws1.sh
@@ -4,6 +4,7 @@
 i3-msg "workspace 1:󰊯; append_layout ~/.config/i3/layouts/ws1.json"
 
 # And finally we fill the containers with the programs they had
-(exec google-chrome-stable &)
 sleep 1
-(exec google-chrome-stable --incognito &)
+(exec firefox &)
+sleep 1
+(exec firefox --private-window &)

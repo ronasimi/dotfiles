47e0057faf8d409f30b4abaded7652193dcf3467
Ron Asimi <rondotasimiatgmaildotcom>
2021-10-18 17:04:05 -0400

    back to google chrome

--- home/.config/i3/scripts/ws1.sh
+++ home/.config/i3/scripts/ws1.sh
@@ -4,6 +4,6 @@
 i3-msg "workspace 1:󰈹; append_layout ~/.config/i3/layouts/ws1.json"
 
 # And finally we fill the containers with the programs they had
-(exec firefox &)
+(exec google-chrome-stable &)
 sleep 1
-(exec firefox -private-window &)
+(exec google-chrome-stable -incognito &)

efbae837d72b09ccd94ceb3c67cae726e1390197
Ron Asimi <rondotasimiatgmaildotcom>
2022-11-02 12:54:43 -0400

    update configs for VScode

--- home/.config/i3/scripts/ws4.sh
+++ home/.config/i3/scripts/ws4.sh
@@ -4,5 +4,5 @@
 i3-msg "workspace 4:󰊢; append_layout ~/.config/i3/layouts/ws4.json"
 
 # And finally we fill the containers with the programs they had
-(exec atom &)
+(exec code &)
 (alacritty --working-directory /home/ron/repos &)

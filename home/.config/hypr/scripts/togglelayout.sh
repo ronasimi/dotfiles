fdc6f16de83a9850c8cf6c26a541247ae4b7868b
Ron Asimi <ron.asimi@gmail.com>
2023-10-07 21:05:17 -0400

    add files for scripts... update workspace icons, change volume change amount for scroll - waybar config

---
+++ home/.config/hypr/scripts/togglelayout.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+if [[ $(hyprctl -j getoption general:layout | jq -r '.str') = "master" ]]; then
+  hyprctl keyword general:layout "dwindle"
+else
+  hyprctl keyword general:layout "master"
+fi

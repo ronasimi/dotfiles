63958b9fc1253a2ca4dc2ca392be51ea1bd9b412
Ron Asimi <ron.asimi@gmail.com>
2023-10-18 21:22:20 -0400

    add Operator Mono Nerd Lig, fonttest.sh

---
+++ home/.bin/fonttest.sh
@@ -0,0 +1,1 @@
+echo -e "->> <<- && || != << >> => -> == <=\n\e[1mthe quick brown fox jumped over the lazy dog\e[0m\n\e[3mthe quick brown fox jumped over the lazy dog\e[0m\n\e[4mthe quick brown fox jumped over the lazy dog\e[0m\n\e[9mthe quick brown fox jumped over the lazy dog\e[0m"

72387cf9ad31094ef265c91395c607557f0f80a6
Ron Asimi <ron.asimi@gmail.com>
2026-06-03 11:18:53 -0400

    update snap setting - hyprland.lua, update chrome flags to get rid of artifacts

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -66,6 +66,11 @@ hl.config({
         -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
         resize_on_border     = true,
         hover_icon_on_border = true,
+        snap = {
+            enabled = true,
+            window_gap = 9,
+            monitor_gap = 9
+        },
 
         -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
         allow_tearing        = false,

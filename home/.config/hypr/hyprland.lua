fd7b58c91543707a7395ff4976e7cdc96a713e42
Ron Asimi <ron.asimi@gmail.com>
2026-06-07 09:21:55 -0400

    change inactive opacity

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -84,7 +84,7 @@ hl.config({
 
         -- Change transparency of focused and unfocused windows
         active_opacity   = 1.0,
-        inactive_opacity = 0.90,
+        inactive_opacity = 0.75,
 
         shadow           = {
             enabled        = true,

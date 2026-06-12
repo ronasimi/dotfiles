79b569ee69bc55712db9f4380decaa3c69b8ca55
Ron Asimi <ron.asimi@gmail.com>
2026-06-12 14:30:22 -0400

    update waybar script for show/hide, added clight config (not currently used)

--- home/.config/hypr/scripts.lua
+++ home/.config/hypr/scripts.lua
@@ -8,10 +8,10 @@ local is_waybar_visible = false -- Set to true because Waybar launches visible
 local waybar_hover_timer = hl.timer(function()
     local pos = hl.get_cursor_pos()
     if pos then
-        if pos.y <= 12 and not is_waybar_visible then
+        if pos.y <= 18 and not is_waybar_visible then
             hl.exec_cmd("pkill -SIGUSR1 '^waybar$'")
             is_waybar_visible = true
-        elseif pos.y > 12 and is_waybar_visible then
+        elseif pos.y > 36 and is_waybar_visible then
             hl.exec_cmd("pkill -SIGUSR1 '^waybar$'")
             is_waybar_visible = false
         end
